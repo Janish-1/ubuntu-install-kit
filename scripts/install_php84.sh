f660ee352f20b6536ccad92cb2c0152b34dda868
Janish-1 <janish.pancholi11@gmail.com>
2025-10-31 13:01:42 +0530

    Added php84

---
+++ scripts/install_php84.sh
@@ -0,0 +1,6 @@
+sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php # Press enter to confirm.
+sudo apt update
+sudo apt install php8.4-cli php8.4-fpm
+sudo apt install php8.4-common php8.4-{bcmath,bz2,curl,gd,gmp,intl,mbstring,opcache,readline,xml,zip}
+# Reload Bashrc
+# source ~/.bashrc
