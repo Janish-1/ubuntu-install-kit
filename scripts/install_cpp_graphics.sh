733f829be22165f4c6ee1dec0f080e64354fdcbf
Janish-1 <janish.pancholi11@gmail.com>
2025-11-24 23:25:58 +0530

    Added GCC Installer for GUI Application

---
+++ scripts/install_cpp_graphics.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install -y build-essential cmake git
+sudo apt install -y qt6-base-dev qt6-base-dev-tools qt6-tools-dev qt6-tools-dev-tools
+qmake6 --version
+sudo apt install -y ffmpeg
+ffmpeg -version
+sudo apt install -y libsqlite3-dev sqlite3
+# git clone https://github.com/cameron314/concurrentqueue.git
+sudo apt install -y libinotifytools0-dev
+sudo apt install -y qtmultimedia5-dev
